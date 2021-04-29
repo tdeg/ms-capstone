6ec9ab454deb74325eb9bb55032593370e4a84e7
Tyler DeGennaro <tylerdegennaro7@gmail.com>
2021-04-28 22:00:49 -0400

    update main.py

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 """
 API entry point.
 """
+import os
 import uvicorn
 
 if __name__ == "__main__":
-    uvicorn.run("app.api:app", host="0.0.0.0", port=8081, reload=True)  # make sure to turn this off
+    if os.environ.get("ENV") == "prod":
+        uvicorn.run("app.api:app", host="0.0.0.0", port=8081)
+    else:
+        uvicorn.run("app.api:app", host="0.0.0.0", port=8081, reload=True)
